8ce0ac02aecb1c32fc53c9226bb26f73d83eb1f1
Rafael Peres <rafaeldasilvaperes@hotmail.com>
2022-12-10 02:37:03 -0300

    Connecting to the cloud database

---
+++ Migrations/20221210052640_changingdb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AluraMusicAPIRest.Migrations
+{
+    public partial class changingdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "productImage",
+                table: "Product",
+                type: "longblob",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "productImage",
+                table: "Product",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(byte[]),
+                oldType: "longblob")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
